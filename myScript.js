7352d6b358cbb68a15493973706df253f5eb8811
Hu22ainaBamanga <hussaina.bamanga@student.neu.edu.ng>
2025-05-04 16:45:43 +0100

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,8 @@
+
+document.getElementById("showpassword").addEventListener("click",showpasswordFunction);
+
+function
+showpasswordFunction() {
+const passwordInput = document.getElementById("password");
+
+}
